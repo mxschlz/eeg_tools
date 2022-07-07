062bdac2e0cbc76e3312e35e1794e6b7f65bcd39
Max Schulz <max.schulz10@web.de>
2022-07-07 14:22:17 +0200

    name change

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name="mne_appendix",
+setup(name="eeg_tools",
       version="0.0.1",
       description="Package to plot, analyze and visualize electrophysiological (EEG) data.",
       py_modules=["mne"],
