5080bfe79f567941b76ffa8e0bfcada1cceeda09
Max Schulz <max.schulz10@web.de>
2022-07-21 14:12:24 +0200

    Implemented sort_evokeds(): reads all evoked files from ids and sorts them in a dict. Optionally returns grand averages.

--- src/analysis.py
+++ src/analysis.py
@@ -97,41 +97,37 @@ def quality_check(ids, fig_size=(60,60), out_folder="D:/EEG/vocal_effort/qc"):
         plt.savefig(pathlib.Path(out_folder) / figures[n])
         plt.close()
 
-def group_evokeds():
-    pass
+
+def sort_evokeds(ids, root_dir, average=True):
+    all_evokeds = dict()
+    for id in ids:
+        evokeds = set.read_object("evokeds", root_dir, id)
+        for condition in evokeds:
+            if condition.comment not in all_evokeds.keys():
+                all_evokeds[condition.comment] = [condition]
+            else:
+                all_evokeds[condition.comment].append(condition)
+    if average == True:
+        evokeds_avrgd = dict()
+        for key in all_evokeds:
+            evokeds_avrgd[key] = mne.grand_average(all_evokeds[key])
+        return all_evokeds, evokeds_avrgd
+    else:
+        return all_evokeds
 
 
 if __name__ == "__main__":
     root_dir = pathlib.Path("D:/EEG")
     header_files = set.find(path=root_dir, mode="pattern", pattern="*.vhdr")
     ids = set.get_ids(header_files)
-    X = epochs.get_data()
-    n_epochs, n_channels, n_times = epochs.get_data().shape
-    X -= np.expand_dims(X.mean(axis=2), axis=2)  # center data on 0
-    X = np.transpose(epochs._data,
-                     (1, 0, 2)).reshape(n_channels,
-                                        n_epochs * n_times).T  # concatenate
-    C0 = X.T @ X  # Data covariance Matrix
-    n_components = 2
-    D, P = np.linalg.eig(C0)  # eigendecomposition of C0
-    idx = np.argsort(D)[::-1][0:n_components]   # sort array
-    # by descending magnitude
-    D = D[idx]
-    P = P[:, idx]  # rotation matrix
-    pca_evokeds = []
-    for cond in epochs.event_id.keys():
-        # use rotation matrix on every single condition
-        n_epochs, n_channels, n_times = epochs[cond]._data.shape
-        X = epochs[cond]._data
-        X -= np.expand_dims(X.mean(axis=2), axis=2)  # center data on 0
-        X = np.transpose(epochs[cond]._data,
-                         (1, 0, 2)).reshape(n_channels, n_epochs * n_times).T
-        Y = X @ P  # get principle components
-        pca = np.reshape(Y.T, [-1, n_epochs, n_times]).transpose([1, 0, 2])
-        pca_evoked = mne.EvokedArray(np.mean(pca, axis=0),
-                                     mne.create_info(
-                                     n_components, epochs[cond].info["sfreq"],
-                                     ch_types="eeg"),
-                                     tmin=epochs[cond].tmin)
-        pca_evoked.pick_channels(ch_names=["0"])
-        pca_evokeds.append(pca_evoked)
+    all_evokeds = dict()
+    for id in ids:
+        evokeds = set.read_object("evokeds", root_dir, id)
+        for condition in evokeds:
+            if condition.comment not in all_evokeds.keys():
+                all_evokeds[condition.comment] = [condition]
+            else:
+                all_evokeds[condition.comment].append(condition)
+    evokeds_avrgd = dict()
+    for key in all_evokeds:
+        evokeds_avrgd[key] = mne.grand_average(all_evokeds[key])
