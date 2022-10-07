026bfe7e636e0d04dd7f3ea6de417989fb9f645f
Max Schulz <max.schulz10@web.de>
2022-10-07 10:57:43 +0200

    create examples

---
+++ example_pipeline_analysis/run_pipeline.py
@@ -0,0 +1,34 @@
+import sys
+import os
+path = "D:\Projects\eeg_tools\src\eeg_tools"
+sys.path.append(path)
+import preprocessing as pre
+import pathlib
+import setup_eeg_tools as set
+import analysis
+import settings
+
+
+# TODO: position _fig_folder somewhere else and make it more dynamic.
+for id in settings.ids[:1]:  # loop through all ids
+    is_preprocessed = False  # set.check_id(id, settings.root_dir)  # check if subject is already preprocessed
+    if is_preprocessed is False:  # if not preprocessed, run loop
+        print(f"START PREPROCESSING PARTICIPANT {settings.ids.index(id)} ({id})")
+        set.generate_folders(root_dir=settings.root_dir, id=id)  # make data folders
+        fig_folder = pathlib.Path(f"D:/EEG/distance_perception/pinknoise/data/{id}/figures")
+        raw = pre.make_raw(settings.header_files, id, fig_folder, settings.mapping, settings.montage)  # make raw object
+        set.save_object(raw, settings.root_dir, id)  # save raw
+        epochs = pre.run_pipeline(raw=raw, fig_folder=fig_folder, config=settings.cfg, exclude_event_id=7)  # run pipeline on raw
+        del raw  # save working memory
+        set.save_object(epochs, settings.root_dir, id)  # save epochs
+        evokeds = pre.make_evokeds(epochs, baseline=(None, 0))  # make evokeds, optionally apply baseline
+        set.save_object(evokeds, settings.root_dir, id)  # save evokeds
+        del epochs, evokeds  # save working memory
+        with open(settings.root_dir / "data" / id / f"config_{id}.txt", "w") as file:
+            file.write(str(settings.cfg))  # save configuration file to keep track of the parameters per subject
+        print(f"PARTICIPANT {settings.ids.index(id)} ({id}) SUCCESSFULLY PREPROCESSED!")
+    else:
+        continue
+
+# After running the pipeline over all participants, do:
+analysis.quality_check(settings.ids, out_folder=settings.root_dir/"qc")
